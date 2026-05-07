53963581a4ed0b474be7a0d5568700e4f600c140
Stefan Filly <stefan.filly@gmx.de>
2026-05-07 12:31:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'techcalc-pro-wrg-desktop-split-v4';
+wrg-output-card-fix;
 const ASSETS = [
   './','./index.html','./manifest.json',
   './css/tokens.css','./css/layout.css','./css/components.css','./css/modules.css',
