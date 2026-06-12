158564316eb320e88abd494ece39c409799accdd
Stefan Filly <268609011+7cpjrm4s4c-ship-it@users.noreply.github.com>
2026-06-12 18:16:43 +0200

    Phase 35G.3

--- js/modules/rainwater/index.js
+++ js/modules/rainwater/index.js
@@ -2,7 +2,7 @@ import config from './config.js';
 import schema from './schema.js';
 import { state, initialState } from './state.js';
 import { calculate } from './logic.js';
-import { results, savedRecords } from './results.js';
+import { results } from './results.js';
 import controller, { bindRainwaterPlatform } from './controller.js';
 import { createPlatformModule } from '../../platform/moduleRuntime/index.js';
 
@@ -13,7 +13,6 @@ export default createPlatformModule({
   initialState,
   calculate,
   results,
-  savedRecords,
   controller,
   bind: bindRainwaterPlatform
 });
