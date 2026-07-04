fcb588acd9c039970547597f66a48a2d71c5e819
Stefan Filly <268609011+7cpjrm4s4c-ship-it@users.noreply.github.com>
2026-07-04 11:18:26 +0200

    Version 1.3.2 Beta 23

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'techcalc-pro-1.3.2-dev.22';
-const CACHE_REVISION = '1.3.2-dev.22-1-3-2-dev-22-final-ui-hardening';
+const CACHE_NAME = 'techcalc-pro-1.3.2-dev.23';
+const CACHE_REVISION = '1.3.2-dev.23-1-3-2-dev-23-theme-regression-fix';
 const ASSETS = [
   './',
   './index.html',
