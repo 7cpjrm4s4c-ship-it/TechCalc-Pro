3d9855d9f5a810102e453738651e95f422dffaa8
Stefan Filly <stefan.filly@gmx.de>
2026-07-13 14:31:43 +0200

    Phase 47C.1: add flooding platform module entrypoint

---
+++ js/modules/flooding-verification/index.js
@@ -0,0 +1,17 @@
+import config from './config.js';
+import schema from './schema.js';
+import { state, initialState } from './state.js';
+import { calculate } from './logic.js';
+import { results } from './results.js';
+import controller from './controller.js';
+import { createPlatformModule } from '../../platform/moduleRuntime/index.js';
+
+export default createPlatformModule({
+  config,
+  schema,
+  state,
+  initialState,
+  calculate,
+  results,
+  controller
+});
