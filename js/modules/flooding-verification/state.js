3028f268bb07f89d302c8ff7159e5b075752658c
Stefan Filly <stefan.filly@gmx.de>
2026-07-13 14:44:32 +0200

    Phase 47C.2: extend flooding surface state

--- js/modules/flooding-verification/state.js
+++ js/modules/flooding-verification/state.js
@@ -7,6 +7,12 @@ export const initialState = {
   featureEnabled: true,
   projectName: '',
   calculationMode: 'flooding',
+  surfaceCategory: 'roof',
+  surfaceName: '',
+  surfaceAreaType: 'metal-roof',
+  surfaceArea: '100',
+  surfaceCs: '1,0',
+  surfaceCm: '0,9',
   rainDurationMode: 'automatic',
   manualRainDuration: '5',
   rainIntensity5: '',
@@ -15,6 +21,7 @@ export const initialState = {
   activeSurfaceId: null,
   surfaces: [],
   importedRainwaterSnapshot: null,
+  importStatus: '',
   results: null
 };
 
