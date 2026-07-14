9fe29ef9420343c89625662de84962cf2b88796b
Stefan Filly <stefan.filly@gmx.de>
2026-07-14 16:09:23 +0200

    Phase 47C.6.3: route module through central calculation adapter

--- js/modules/flooding-verification/index.js
+++ js/modules/flooding-verification/index.js
@@ -1,7 +1,7 @@
 import config from './config.js';
 import schema from './schema.js';
 import { state, initialState } from './state.js';
-import { calculate } from './logic.js';
+import { calculate } from './calculationAdapter.js';
 import { results } from './results.js';
 import controller, {
   buildFloodingSurfaceRecord,
