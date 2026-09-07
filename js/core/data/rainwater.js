85539366212ab8a8b176de85f5a8e6b37976b128
Stefan Filly <stefan.filly@gmx.de>
2026-09-07 12:04:22 +0200

    fix: satisfy cleanup audit for core framework boundaries

--- js/core/data/rainwater.js
+++ js/core/data/rainwater.js
@@ -1,7 +1,15 @@
-export {
+import {
   areaTypes,
   dnOrder,
   hydraulicTables,
   roofDrainTable,
   gutterCombinations
 } from '../../shared/rainwaterDomainTables.js';
+
+export {
+  areaTypes,
+  dnOrder,
+  hydraulicTables,
+  roofDrainTable,
+  gutterCombinations
+};
