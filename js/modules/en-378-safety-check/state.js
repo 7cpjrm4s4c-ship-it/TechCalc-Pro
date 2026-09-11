b90a5671327af0372c88e9230f2024b3cc5690dc
Stefan Filly <stefan.filly@gmx.de>
2026-09-11 14:15:41 +0200

    refactor: migrate en378 state data to core

--- js/modules/en-378-safety-check/state.js
+++ js/modules/en-378-safety-check/state.js
@@ -1,5 +1,5 @@
 import { createModuleState } from '../../core/state.js';
-import { getDataVersions } from '../../utils/refrigerants/index.js';
+import { getDataVersions } from '../../core/data/index.js';
 
 export const EN_378_SAFETY_CHECK_SCHEMA_VERSION = 1;
 
