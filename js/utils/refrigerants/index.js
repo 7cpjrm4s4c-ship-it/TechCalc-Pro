3dbb6c2ca45f737bc57584f48413b720f81d1db2
Stefan Filly <stefan.filly@gmx.de>
2026-09-13 18:11:24 +0200

    refactor: reduce refrigerant utils to core bridges

--- js/utils/refrigerants/index.js
+++ js/utils/refrigerants/index.js
@@ -1,16 +1,2 @@
-export {
-  getDataVersions,
-  getDataStatus,
-  listRefrigerants,
-  getRefrigerant,
-  getGwp,
-  listSafetyClasses,
-  getSafetyClass,
-  listRegulations,
-  createRegulatoryContext,
-  evaluateRegulations,
-  getApplicableRegulations,
-  listEN378SafetyData,
-  getEN378SafetyData
-} from './refrigerant-service.js';
-export { formatRefrigerantLabel } from './refrigerant-label.js';
+export * from '../../core/data/refrigerants.js';
+export { default } from '../../core/data/refrigerants/refrigerant-service.js';
