ee9329ec4d139e3899aed1fec254e71dc128039b
Stefan Filly <stefan.filly@gmx.de>
2026-09-13 14:15:57 +0200

    chore: move unit conversion data to core data

--- js/core/data/index.js
+++ js/core/data/index.js
@@ -3,5 +3,5 @@ export * from './rainwater.js';
 export * from './pipes.js';
 export * from './refrigerants.js';
 export * from './fGasesSystemSnapshot.js';
-export { convert, unitCategories } from '../../utils/units.js';
+export { convert, unitCategories } from './units.js';
 export { MEDIA, airDensity, getMedium, heatingCooling, ventilation } from '../../utils/calculations.js';
