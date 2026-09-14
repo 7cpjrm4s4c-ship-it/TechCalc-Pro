0a2bc8825f2dc245e8711a5873a97596c6e52e30
Stefan Filly <stefan.filly@gmx.de>
2026-09-14 14:11:05 +0200

    chore: remove obsolete platform module renderer bridge

--- js/platform/moduleRenderer/index.js
+++
@@ -1,8 +0,0 @@
-export {
-  renderPlatformForm,
-  renderPlatformSaved,
-  renderPlatformResultsAndSaved,
-  renderPlatformResults,
-  renderPlatformModuleView,
-  default
-} from '../../core/moduleRenderer.js';
