f3de1faaa1379cb1aff7b5fcb97b05e3d640bfe2
Stefan Filly <stefan.filly@gmx.de>
2026-09-19 10:49:11 +0200

    fix(ux): update settings logger path

--- core/ux/settingsController.js
+++ core/ux/settingsController.js
@@ -1,4 +1,4 @@
-import { logger } from '../logger.js';
+import { logger } from '../diagnostics/logger.js';
 import { trackGlobalEventListener } from '../events/index.js';
 import { initializeUnsavedWorkGuard } from '../storage/unsavedWorkGuard.js';
 
