19cdfd3eb0acbafa5b0548874aac4bf86d828629
Stefan Filly <stefan.filly@gmx.de>
2026-09-20 16:51:17 +0200

    fix(state): avoid duplicate staged modules

--- core/state/state.js
+++
@@ -1,8 +0,0 @@
-import { createStore, registerModuleStore } from './centralStore.js';
-
-let anonymousStoreIndex = 0;
-
-export function createModuleState(initial = {}, options = {}) {
-  const moduleId = options.moduleId || `anonymous-${++anonymousStoreIndex}`;
-  return registerModuleStore(moduleId, createStore(initial, { moduleId }));
-}
