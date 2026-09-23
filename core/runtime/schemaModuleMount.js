9ede32e35648de7a639d0c3500960b6eaae30c97
Stefan Filly <stefan.filly@gmx.de>
2026-09-23 11:07:24 +0200

    fix(core): update schema module mount renderer import

--- core/runtime/schemaModuleMount.js
+++ core/runtime/schemaModuleMount.js
@@ -1,6 +1,6 @@
 import { renderModuleShell } from '../renderer.js';
 import { mountModule } from '../mount.js';
-import { createSchemaView } from '../schemaRenderer.js';
+import { createSchemaView } from '../ui/schemaRenderer.js';
 import { defineModuleDefinition } from '../contracts/index.js';
 export function createSchemaModule(definition) {
   const moduleDefinition = defineModuleDefinition(definition);
