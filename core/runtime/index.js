75d518db2dd959288cb63ba1133ae4d617c9e501
Stefan Filly <stefan.filly@gmx.de>
2026-09-24 19:48:28 +0200

    fix(core): update runtime navigation exports

--- core/runtime/index.js
+++ core/runtime/index.js
@@ -1,11 +1,11 @@
 export * from '../moduleRuntime.js';
 export * from '../moduleLifecycleAdapter.js';
 export * from '../mount.js';
-export * from '../navigation.js';
+export * from '../navigation/index.js';
 export * from './platformLifecycle.js';
 export * from './platformModuleRuntime.js';
 export * from '../registry.js';
-export * from '../router.js';
+export * from '../navigation/router.js';
 export * from './schemaModuleMount.js';
 export { createLineSectionController } from '../lineSectionController/index.js';
 export {
